3c5ee8c22ed2710a2c64a58b84aba89d973a12c1
Yike Qi <data.chirac@gmail.com>
2019-05-02 02:07:44 +0000

    =established simple segmentation/collection after EAST line outputs

---
+++ src/wordsegmentation/WordSegmentation.py
@@ -0,0 +1,144 @@
+import math
+import cv2
+import numpy as np
+
+def non_max_suppression_wh(boxes, probs=None, overlapThresh=0.5):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	boxes=np.array(boxes)
+	# if the bounding boxes are integers, convert them to floats -- this
+	# is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 0]+boxes[:, 2]
+	y2 = boxes[:, 1]+boxes[:, 3]
+
+	# compute the area of the bounding boxes and grab the indexes to sort
+	# (in the case that no probabilities are provided, simply sort on the
+	# bottom-left y-coordinate)
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = y2
+
+	# if probabilities are provided, sort on them instead
+	if probs is not None:
+		idxs = probs
+
+	# sort the indexes
+	idxs = np.argsort(idxs)
+
+	# keep looping while some indexes still remain in the indexes list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the index value
+		# to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the largest (x, y) coordinates for the start of the bounding
+		# box and the smallest (x, y) coordinates for the end of the bounding
+		# box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have overlap greater
+		# than the provided overlap threshold
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+	pick=sorted(pick)     
+	# return only the bounding boxes that were picked
+	return boxes[pick].astype("int"),pick
+
+
+
+
+def wordSegmentation(args,img, kernelSize=25, sigma=11, theta=7, minArea=0): #yike: added args
+	"""Scale space technique for word segmentation proposed by R. Manmatha: http://ciir.cs.umass.edu/pubfiles/mm-27.pdf
+	
+	Args:
+		img: grayscale uint8 image of the text-line to be segmented.
+		kernelSize: size of filter kernel, must be an odd integer.
+		sigma: standard deviation of Gaussian function used for filter kernel.
+		theta: approximated width/height ratio of words, filter function is distorted by this factor.
+		minArea: ignore word candidates smaller than specified area.
+		
+	Returns:
+		List of tuples. Each tuple contains the bounding box and the image of the segmented word.
+	"""
+
+	# apply filter kernel
+	kernel = createKernel(kernelSize, sigma, theta)
+	imgFiltered = cv2.filter2D(img, -1, kernel, borderType=cv2.BORDER_REPLICATE).astype(np.uint8)
+	(_, imgThres) = cv2.threshold(imgFiltered, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	imgThres = 255 - imgThres
+
+	# find connected components. OpenCV: return type differs between OpenCV2 and 3
+	if cv2.__version__.startswith('3.'):
+		(_, components, _) = cv2.findContours(imgThres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	else:
+		(components, _) = cv2.findContours(imgThres, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+	# append components to result
+	res = []
+	for c in components:
+		# skip small word candidates
+		if cv2.contourArea(c) < minArea:
+			continue
+		# append bounding box and image of word to result list
+		currBox = cv2.boundingRect(c) # returns (x, y, w, h)
+		(x, y, w, h) = currBox
+		currImg = cv2.transpose(cv2.resize(img[y:y+h, x:x+w], (args.imgsize[0],args.imgsize[1]), interpolation=cv2.INTER_CUBIC))#img[y:y+h, x:x+w] # revised a little, revise to argsize and transpose, yike
+		res.append((currBox, currImg))
+
+	# return list of words, sorted by x-coordinate
+	return sorted(res, key=lambda entry:entry[0][0])
+
+
+def prepareImg(img, height):
+	"""convert given image to grayscale image (if needed) and resize to desired height"""
+	assert img.ndim in (2, 3)
+	if img.ndim == 3:
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	h = img.shape[0]
+	factor = height / h
+	return cv2.resize(img, dsize=None, fx=factor, fy=factor)
+
+
+def createKernel(kernelSize, sigma, theta):
+	"""create anisotropic filter kernel according to given parameters"""
+	assert kernelSize % 2 # must be odd size
+	halfSize = kernelSize // 2
+	
+	kernel = np.zeros([kernelSize, kernelSize])
+	sigmaX = sigma
+	sigmaY = sigma * theta
+	
+	for i in range(kernelSize):
+		for j in range(kernelSize):
+			x = i - halfSize
+			y = j - halfSize
+			
+			expTerm = np.exp(-x**2 / (2 * sigmaX) - y**2 / (2 * sigmaY))
+			xTerm = (x**2 - sigmaX**2) / (2 * math.pi * sigmaX**5 * sigmaY)
+			yTerm = (y**2 - sigmaY**2) / (2 * math.pi * sigmaY**5 * sigmaX)
+			
+			kernel[i, j] = (xTerm + yTerm) * expTerm
+
+	kernel = kernel / np.sum(kernel)
+	return kernel
