6ac99e0833e70657859f7a471fcb1d44c2e77347
Yike Qi <data.chirac@gmail.com>
2019-06-07 00:48:30 +0000

    update maxsuppression for cv2 east

--- src/wordsegmentation/WordSegmentation.py
+++ src/wordsegmentation/WordSegmentation.py
@@ -65,7 +65,74 @@ def non_max_suppression_wh(boxes, probs=None, overlapThresh=0.5):
 	# return only the bounding boxes that were picked
 	return boxes[pick].astype("int"),pick
 
+def non_max_suppression_merge(boxes, probs=None, overlapThresh=0.5):
+	# if there are no boxes, return an empty list# not implemented
+	if len(boxes) == 0:
+		return []
+	boxes=np.array(boxes)
+	# if the bounding boxes are integers, convert them to floats -- this
+	# is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
 
+	# initialize the list of picked indexes
+	pick = []
+	merged_boxes=[] #yike qi
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	# compute the area of the bounding boxes and grab the indexes to sort
+	# (in the case that no probabilities are provided, simply sort on the
+	# bottom-left y-coordinate)
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = y2
+
+	# if probabilities are provided, sort on them instead
+	if probs is not None:
+		idxs = probs
+
+	# sort the indexes
+	idxs = np.argsort(idxs)
+
+	# keep looping while some indexes still remain in the indexes list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the index value
+		# to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the largest (x, y) coordinates for the start of the bounding
+		# box and the smallest (x, y) coordinates for the end of the bounding
+		# box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have overlap greater
+		# than the provided overlap threshold
+		sel=np.concatenate(([last],np.where(overlap > overlapThresh)[0]))
+		idxsel=idxs[sel]
+		#print(min(x1[idxsel]))#[idxsel])
+		merged_boxes.append((min(x1[idxsel]),min(y1[idxsel]),
+                     max(x2[idxsel]),max(y2[idxsel])))  
+		idxs = np.delete(idxs, sel)
+	#pick=sorted(pick)     
+	merged_boxes=sorted(merged_boxes, key=lambda element: (element[1], element[0]))
+	# return only the bounding boxes that were picked
+	return np.array(merged_boxes).astype("int")#boxes[pick].astype("int"),pick
 
 
 def wordSegmentation(args,img, kernelSize=25, sigma=11, theta=7, minArea=0): #yike: added args
